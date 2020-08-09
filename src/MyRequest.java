82afb3f4d51bac61da6afa5ea423de0cb2e6b442
Lirson <madison00@126.com>
2020-08-09 19:52:23 +0800

    Initial commit

---
+++ src/MyRequest.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.InputStream;
+//请求对象
+public class MyRequest {
+    private String url;
+    private String method;
+    public MyRequest(InputStream inputStream) throws IOException{
+        String httpRequest="";
+        byte[] httpRequestBytes=new byte[1024];
+        int length=0;
+        if((length=inputStream.read(httpRequestBytes))>0){
+            httpRequest=new String(httpRequestBytes,0,length);
+        }
+        //对HTTP请求协议进行解析
+        String httpHead=httpRequest.split("\n")[0];
+        url=httpHead.split("\\s")[1];
+        method=httpHead.split("\\s")[0];
+        System.out.println(this);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+}
---
+++ src/ServletMappingConfig.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServletMappingConfig {
+    public static List<ServletMapping> servletMappingList=new ArrayList<>();
+    static {
+        servletMappingList.add(new ServletMapping("helloworld","/world","HelloWorldServlet"));
+    }
+}
